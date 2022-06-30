6c6d4026de1197ae4b4f2e5a1756f42a36c629ec
Harry <Hazmclean92@gmail.com>
2022-06-30 17:04:53 +1000

    First commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './app'
+
+import './index.css'
+
+// ==== REACT 17 OR OLDER ==========
+import ReactDOM from 'react-dom';
+ReactDOM.render(<App/>, document.querySelector("#root"))
+
+
